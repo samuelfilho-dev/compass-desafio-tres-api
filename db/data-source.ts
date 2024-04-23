574924647924519619c41ff31def6f45cadbde70
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-04-23 20:34:25 -0300

    feat: add database config

---
+++ db/data-source.ts
@@ -0,0 +1,44 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+export let dataSourceOptions: DataSourceOptions;
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    dataSourceOptions = {
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'root',
+      username: 'postgres',
+      entities: ['**/*.entity.js'],
+      database: 'desafio_tres',
+      synchronize: true,
+      logging: true,
+      // migrations: ['migrations/**'],
+    };
+    break;
+  case 'test':
+    dataSourceOptions = {
+      type: 'sqlite',
+      database: 'test.sqlite',
+      entities: ['**/*.entity.ts'],
+      synchronize: true,
+    };
+    break;
+  case 'production':
+    dataSourceOptions = {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      migrationsRun: true,
+      entities: ['**/*.entity.js'],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    };
+    break;
+  default:
+    throw new Error('unknown environment');
+}
+
+const dataSource = new DataSource(dataSourceOptions);
+export default dataSource;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { CategoryModule } from './category/category.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from '../db/data-source';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
+    ProductModule,
+    CategoryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
