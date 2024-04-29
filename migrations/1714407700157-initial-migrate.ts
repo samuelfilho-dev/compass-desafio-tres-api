dc9d210543a137824e94842aa0594e9d8e4a0206
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-04-29 15:10:30 -0300

    fix: create new migrate file

--- migrations/1714407700157-initial-migrate.ts
+++ migrations/1714407700157-initial-migrate.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class InitialMigrate1714404456391 implements MigrationInterface {
-    name = 'InitialMigrate1714404456391'
+export class InitialMigrate1714407700157 implements MigrationInterface {
+    name = 'InitialMigrate1714407700157'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "category" ("id" SERIAL NOT NULL, "name" character varying(50) NOT NULL, "image_link" character varying(250) NOT NULL, "created_date" TIMESTAMP NOT NULL DEFAULT now(), "updated_date" TIMESTAMP NOT NULL DEFAULT now(), "productId" uuid, CONSTRAINT "PK_9c4e4a89e3674fc9f382d733f03" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "product" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(50) NOT NULL, "sku" character varying(10) NOT NULL, "description" character varying(250) NOT NULL, "large_description" character varying NOT NULL, "price" integer NOT NULL, "discount_price" integer NOT NULL, "discount_percent" integer NOT NULL, "is_new" boolean NOT NULL DEFAULT false, "imageLink" character varying(250) NOT NULL, "other_images_link" character varying NOT NULL, "create_date" TIMESTAMP NOT NULL DEFAULT now(), "update_date" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_bebc9158e480b949565b4dc7a82" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "product" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(50) NOT NULL, "sku" character varying(10) NOT NULL, "description" character varying(250) NOT NULL, "large_description" character varying NOT NULL, "price" integer NOT NULL, "discount_price" integer NOT NULL, "discount_percent" integer NOT NULL, "is_new" boolean NOT NULL, "imageLink" character varying(250) NOT NULL, "other_images_link" character varying NOT NULL, "create_date" TIMESTAMP NOT NULL DEFAULT now(), "update_date" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_bebc9158e480b949565b4dc7a82" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "category" ADD CONSTRAINT "FK_5f4035564515762e47d19334f23" FOREIGN KEY ("productId") REFERENCES "product"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
